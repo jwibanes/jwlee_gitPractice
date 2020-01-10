0b1893f39c4b14528b4f1ba4504b3f49c6a8753f
Jungwoo Lee <jwibanes@naver.com>
2020-01-10 23:16:39 +0900

    Practice code 0001, sampling without replacement

--- Practice_Python_Codes.py
+++ Practice_Python_Codes.py
@@ -1,2 +1,18 @@
 #This file is created for practicing basic codes in python
 
+
+#Picking numbers using sampling without replacement
+#A function to pick 3 random integers from 1 to 10
+from random import randint
+
+def sample_without_replacement():
+    numbers = []
+    while len(numbers) < 3:
+        picked_number = randint(1,10)
+        if picked_number not in numbers:
+            numbers.append(picked_number)
+
+    return(numbers)
+
+#Code to execute function
+print(sample_without_replacement())
